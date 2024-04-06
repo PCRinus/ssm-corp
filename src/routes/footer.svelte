76f3f388273f8a1f8e728ba0336da1f9b4f7bf26
Mircea Casapu <mircea.casapu@gmail.com>
2024-04-06 15:11:11 +0300

    feat: add gdpr disclaimer page

--- src/routes/footer.svelte
+++ src/routes/footer.svelte
@@ -2,7 +2,9 @@
 	import logoBlack from '$lib/assets/logo-black.png';
 </script>
 
-<footer class="bg-primary border-primary flex flex-shrink-0 flex-col gap-3 px-6 py-4 text-black rounded-t-md">
+<footer
+	class="flex flex-shrink-0 flex-col gap-3 rounded-t-md border-primary bg-primary px-6 py-4 text-black"
+>
 	<div>
 		<img class="aspect-square h-24" alt="The project logo" src={logoBlack} />
 	</div>
@@ -12,7 +14,7 @@
 		<h2>
 			<span class="font-semibold">C.U.I: </span>12345678
 		</h2>
-    <h2>
+		<h2>
 			<span class="font-semibold">Registrul Comerțului: </span>J35/1234/2021
 		</h2>
 	</div>
@@ -29,6 +31,7 @@
 	<div class="mt-2 flex flex-col gap-2">
 		<h3 class="text-lg font-semibold">Linkuri utile</h3>
 		<div class="flex flex-col">
+			<a class="text-base underline" href="/gdpr">Politica GDPR</a>
 			<a class="text-base underline" href="https://www.inspectiamuncii.ro/web/itm-timis/"
 				>ITM Timis</a
 			>
---
+++ src/routes/gdpr/+page.svelte
@@ -0,0 +1,41 @@
+<div class="flex flex-col gap-6">
+	<h1 class="text-3xl font-semibold">Politică GDPR</h1>
+	<div class="flex flex-col gap-2">
+		<h2 class="text-2xl font-semibold">Cine suntem și cum ne puteți contacta?</h2>
+
+		<p>
+			SSM Corp este denumirea comerciala a societății SC CD Safety Core SRL, cu sediul în Timisoara,
+			str. Nicolae Labis bloc 3, camera 1, având CUI 41609893, denumită în continuare „SSM Corp” sau
+			„Societatea”.
+		</p>
+
+		<p>
+			În sensul legislației cu privire la protecția datelor, suntem operator atunci când prelucrăm
+			datele dvs cu caracter personal. Întrucât suntem întotdeauna deschiși să aflăm opiniile
+			dumneavoastră, precum și să vă furnizăm orice informații suplimentare de care ați putea avea
+			nevoie cu privire la prelucrarea datelor dvs, vă încurajăm să ne la adresa de e-mail [INSERT
+			office EMAIL].
+		</p>
+	</div>
+
+	<div class="flex flex-col gap-2">
+		<h2 class="text-2xl font-semibold">Cum utilizăm datele cu caracter personal?</h2>
+
+		<p>
+			Utilizam informațiile cu caracter personal cu scopul de a furniza serviciile promise în mod
+			transparent și legal. În acest sens, folosim datele cu caracter personal pentru a transmite
+			produsele în format digital, pentru a elibera documentele obligatorii, precum și alte
+			operațiuni necesare pentru a furniza cu succes obiectul activității.
+		</p>
+	</div>
+
+	<div class="flex flex-col gap-2">
+		<h2 class="text-2xl font-semibold">Care sunt drepturile dumneavoastră?</h2>
+		<p>
+			În orice moment ne puteți solicita datele dumneavoastră cu caracter personal pe care le avem
+			stocate. De asemenea, ne puteți solicita eliminarea acestora din baza noastră de date. Pentru
+			orice fel de informații suplimentare legate de cum utilizăm datele cu caracter personal sau
+			solicitări în acest sens, ne puteți contacta la adresa de e-mail [INSERT OFFICE EMAIL HERE]
+		</p>
+	</div>
+</div>
